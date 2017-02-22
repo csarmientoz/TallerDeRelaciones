f31256efe2c840dc15c3a68c920f953f2f884561
Cristian Sarmiento <Cristian Sarmiento@CRISTIAN>
2017-02-22 11:14:10 -0500

    Casa con poligonos

---
+++ Poo/Poo/src/ejerciciopoo/Triangulo.java
@@ -0,0 +1,15 @@
+
+package ejerciciopoo;
+
+import ch.aplu.turtle.Turtle;
+
+
+public class Triangulo {
+    private int lado;
+
+    public Triangulo(int lado) {
+        this.lado = lado;
+    }
+   public void dibujar(Turtle turtle){
+   }
+}
